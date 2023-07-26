8093d30bc6516aead28cf8c1b654d9fa3c531a84
Farzad Abdolhosseini <farzad@fixie.ai>
2023-07-26 09:37:54 -0700

    load mike chunks: 6x1000

--- packages/webapp/src/app/api/podcasts/query/route.tsx
+++ packages/webapp/src/app/api/podcasts/query/route.tsx
@@ -6,7 +6,7 @@ import {
   Corpus,
   DefaultFormatter,
   DocsQAProps,
-  FixieCorpus,
+  // FixieCorpus,
   ScoredChunk
 } from 'ai-jsx/batteries/docs'
 import { OpenAI } from 'ai-jsx/lib/openai'
@@ -19,26 +19,90 @@ import { NextRequest } from 'next/server'
 import { Jsonifiable } from 'type-fest'
 import { StreamingTextResponse } from 'ai'
 
-const ChunkFormatter = ({ doc }: { doc: ScoredChunk<PodcastMetadata> }) => (
-  <>
-    {/* Title: {doc.chunk.documentName ?? 'Untitled'} */}
-    Source: {doc.chunk.metadata?.source}
-    {'\n'}
-    Content: {doc.chunk.content}
-  </>
-)
-
 import { JsonObject } from 'type-fest'
 
 type MustBeJsonObject<T> = T extends JsonObject ? T : never
 type PodcastMetadata = MustBeJsonObject<{ source: string }>
 
-async function PodcastDocsQA(props: DocsQAProps<PodcastMetadata>) {
-  let chunks
+export class FixieCorpus<ChunkMetadata extends Jsonifiable = Jsonifiable>
+  implements Corpus<ChunkMetadata>
+{
+  private static readonly DEFAULT_FIXIE_API_URL = 'https://app.fixie.ai/api'
+
+  private readonly fixieApiUrl: string
+
+  constructor(
+    private readonly corpusId: string,
+    private readonly fixieApiKey?: string
+  ) {
+    if (!fixieApiKey) {
+      this.fixieApiKey = process.env['FIXIE_API_KEY']
+      if (!this.fixieApiKey) {
+        throw new Error(
+          'You must provide a Fixie API key to access Fixie corpora. Find yours at https://app.fixie.ai/profile.'
+        )
+      }
+    }
+    this.fixieApiUrl =
+      process.env['FIXIE_API_URL'] ?? FixieCorpus.DEFAULT_FIXIE_API_URL
+  }
+
+  async search(
+    query: string,
+    params?: { limit?: number; metadata_filter?: any }
+  ): Promise<ScoredChunk<ChunkMetadata>[]> {
+    console.log('Hey there!')
+    const response = await fetch(
+      `${this.fixieApiUrl}/corpora/${this.corpusId}:query`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${this.fixieApiKey}`
+        },
+        body: JSON.stringify({
+          query_string: query,
+          chunk_limit: params?.limit,
+          metadata_filter: params?.metadata_filter
+        })
+      }
+    )
+    if (response.status !== 200) {
+      throw new Error(
+        `Fixie API returned status ${response.status}: ${await response.text()}`
+      )
+    }
+    const apiResults = await response.json()
+    return apiResults.chunks.map((result: any) => ({
+      chunk: {
+        content: result.content,
+        metadata: result.metadata,
+        documentName: result.document_name
+      },
+      score: result.score
+    }))
+  }
+}
+
+const ChunkFormatter = ({ doc }: { doc: ScoredChunk<PodcastMetadata> }) => (
+  <>
+    {`\n\`\`\`transcript source="${doc.chunk.metadata?.source}" \n`}
+    {doc.chunk.content}
+    {'\n```\n'}
+  </>
+)
+
+async function PodcastDocsQA(props: {
+  corpus: FixieCorpus
+  question: string
+  chunkLimit?: number
+}) {
+  let chunks: ScoredChunk<PodcastMetadata>[]
   try {
-    chunks = await props.corpus.search(props.question, {
-      limit: props.chunkLimit ?? 1
-    })
+    chunks = (await props.corpus.search(props.question, {
+      limit: props.chunkLimit ?? 6,
+      metadata_filter: { chunk_size: 1000 }
+    })) as ScoredChunk<PodcastMetadata>[]
   } catch (e: any) {
     return <>Sorry there was an error. {e.message}</>
   }
@@ -52,9 +116,10 @@ async function PodcastDocsQA(props: DocsQAProps<PodcastMetadata>) {
         <ChatCompletion>
           <SystemMessage>
             You are a helpful chatbot that answers questions about a podcast
-            episode by looking at its transcript. The relevant parts of the
-            transcript is given to you below, and you are to answer the user
-            question only based on what has been said in the podcast.
+            episode by looking at its transcript. You have managed to find the
+            transcripts below that might or might not be relevant to the
+            question. You must answer the user question only based on what the
+            retrieved documents say.
             {/* The transcripts are relevant pieces from the podcast transcript. */}
           </SystemMessage>
           <SystemMessage>
@@ -79,9 +144,14 @@ async function PodcastDocsQA(props: DocsQAProps<PodcastMetadata>) {
           </SystemMessage>
           <SystemMessage>
             Here are the relevant transcript texts:
-            {'\n```txt filename="transcripts.txt"\n'}
-            {chunks.map(chunk => chunkFormatter({ doc: chunk }))}
-            {'\n```\n'}
+            {/* {'\n```txt filename="transcripts.txt"\n'} */}
+            {'\n'}
+            ------
+            {chunks.reverse().map(chunk => chunkFormatter({ doc: chunk }))}
+            ------
+            {/* TODO: closest on the bottom */}
+            {'\n'}
+            {/* {'\n```\n'} */}
             The user question follows below.
           </SystemMessage>
           <UserMessage>{props.question}</UserMessage>
@@ -104,15 +174,17 @@ async function DocsAgent({
   question: string
   corpusId: string
 }) {
-  const corpus = new FixieCorpus(corpusId) as Corpus<PodcastMetadata>
+  const corpus = new FixieCorpus(corpusId)
   return <PodcastDocsQA question={question} corpus={corpus} />
 }
 
 export async function POST(request: NextRequest) {
   const json = await request.json()
   const question = json.messages[json.messages.length - 1]
-  const corpusId = json.corpusId
+  // const corpusId = json.corpusId
+  const corpusId = '3'
 
+  // @ts-ignore
   if (corpusId == null || corpusId == '') {
     toTextStream(
       <div>
